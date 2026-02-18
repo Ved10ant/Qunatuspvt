51df96436eb582e89af39e19b51d848f7f9e247d
Vedant Dighe <vedantdighe2707@gmail.com>
2026-02-18 18:05:38 +0530

    fix: resolve JSX namespace lint error

--- QuantasFrontend/src/plugins/MathPlugin.tsx
+++ QuantasFrontend/src/plugins/MathPlugin.tsx
@@ -1,5 +1,5 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import { useEffect } from 'react';
+import { useEffect, type JSX } from 'react';
 import { $createMathNode, MathNode } from '../nodes/MathNode';
 import { $getSelection, $isRangeSelection, COMMAND_PRIORITY_EDITOR, createCommand } from 'lexical';
 import type { LexicalCommand } from 'lexical';
